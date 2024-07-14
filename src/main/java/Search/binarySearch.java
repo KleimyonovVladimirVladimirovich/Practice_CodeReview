10cbb16f18bd50318107caa05aa381f042b863b2
VladimirTensh <kitkatorgnew@gmail.com>
2024-07-14 21:50:24 +0300

    Solution

--- src/main/java/Search/binarySearch.java
+++ src/main/java/Search/binarySearch.java
@@ -2,33 +2,28 @@ package Search;
 
 public class binarySearch {
     public static void main(String[] args) {
-        int index = BinarySearch(new int[]{89, 57, 91, 47, 95, 3, 27, 22, 67, 99}, 27);
+        int index = recursiveBinarySearch(new int[]{89, 57, 91, 47, 95, 3, 27, 22, 67, 99},0,10, 27);
         print(27, index);
     }
-    public static int BinarySearch(int arr[], int elementToSearch) {
+    public static int recursiveBinarySearch(int[] arr, int firstElement, int lastElement, int elementToSearch) {
 
-        int firstIndex = 0;
-        int lastIndex = arr.length - 1;
+        // условие прекращения
+        if (lastElement >= firstElement) {
+            int mid = firstElement + (lastElement - firstElement) / 2;
 
-        // условие прекращения (элемент не представлен)
-        while(firstIndex <= lastIndex) {
-            int middleIndex = (firstIndex + lastIndex) / 2;
             // если средний элемент - целевой элемент, вернуть его индекс
-            if (arr[middleIndex] == elementToSearch) {
-                return middleIndex;
-            }
+            if (arr[mid] == elementToSearch)
+                return mid;
 
-            // если средний элемент меньше
-            // направляем наш индекс в middle+1, убирая первую часть из рассмотрения
-            else if (arr[middleIndex] < elementToSearch)
-                firstIndex = middleIndex + 1;
+            // если средний элемент больше целевого
+            // вызываем метод рекурсивно по суженным данным
+            if (arr[mid] > elementToSearch)
+                return recursiveBinarySearch(arr, firstElement, mid - 1, elementToSearch);
 
-                // если средний элемент больше
-                // направляем наш индекс в middle-1, убирая вторую часть из рассмотрения
-            else if (arr[middleIndex] > elementToSearch)
-                lastIndex = middleIndex - 1;
-
+            // также, вызываем метод рекурсивно по суженным данным
+            return recursiveBinarySearch(arr, mid + 1, lastElement, elementToSearch);
         }
+
         return -1;
     }
     public static void print(int elementToSearch, int index) {
