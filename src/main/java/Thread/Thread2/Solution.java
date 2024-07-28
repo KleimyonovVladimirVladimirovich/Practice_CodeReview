dc00bd7d5b021fb28750d0d3bad44979ec50f124
VladimirTensh <kitkatorgnew@gmail.com>
2024-07-29 00:23:46 +0300

    initial

--- src/main/java/Thread/Thread2/Solution.java
+++ src/main/java/Thread/Thread2/Solution.java
@@ -4,7 +4,10 @@ import static java.lang.Thread.*;
 
 //Проблематика многопоточности, synchronize, volatile, yield, join, deadlock, условия синх памяти
 public class Solution  {
-    public static void main(String[] args) throws  InterruptedException{
-
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("A");
+        sb.append(" B");
+        System.out.println(sb);
     }
 }
---
+++ src/main/java/Thread/Thread2/Stock.java
@@ -0,0 +1,41 @@
+package Thread.Thread2;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stock {
+    public volatile static Integer fee = 0; //komissia
+
+    public static final Object monitor = new Object(); //вот так всегда должен выглядеть мютабл (static final)
+
+    public static void main(String[] args) throws InterruptedException {
+
+        List<Task> tasks = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            Task task = new Task();
+            tasks.add(task);
+            task.start();
+            System.out.println(Stock.fee);
+
+        }
+        System.out.println("All started");
+        for (Task task: tasks) {
+           task.join();
+        }
+        System.out.println(fee);
+        System.out.println("end");
+    }
+
+}
+class Task extends Thread {
+    public void run() {
+        //Class<Stock> stockClass = Stock.class;
+        synchronized (Stock.monitor) { //Ждет объект монитора (не принимает примитивы)
+            Stock.fee+=getOneFee();
+        }
+
+       // Stock.fee+=getOneFee(); //блок мьютабл
+    }
+    private Integer getOneFee() {
+        return 1;
+    }
+}
